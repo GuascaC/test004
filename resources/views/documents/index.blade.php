5831cdadf6ed4893a2878b4274c5072c67c570a7
CGuasca <mauricioguasca11@gmail.com>
2023-05-16 15:33:26 -0500

    16/05

---
+++ resources/views/documents/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div col-sm-4>
+                <div class="card text-white bg-light">
+                  <div class="card-body">
+                    <a href="#">
+                        <img class="img-doc" src="https://penstagram.theowlclub.net/uploads/default/original/3X/6/e/6e0afb02d0a70cb90ca9b17584e9227fef0a47e0.jpeg" alt="">
+                    </a>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
